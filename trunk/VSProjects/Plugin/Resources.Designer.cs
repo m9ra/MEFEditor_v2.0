ad8378d37da2e11ec70717dbb84a09be2bc3ff9e
m9ra <m9racent@centrum.cz>
2014-07-10 17:51:57 +0200

    Plugin Icon change + thesis changes according to last meeting

--- trunk/VSProjects/Plugin/Resources.Designer.cs
+++ trunk/VSProjects/Plugin/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace MEFEditor.Plugin {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace MEFEditor.Plugin {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MEFEditor.Plugin.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace MEFEditor.Plugin {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace MEFEditor.Plugin {
         /// <summary>
         ///   Looks up a localized string similar to Can not create tool window..
         /// </summary>
-        internal static string CanNotCreateWindow {
+        public static string CanNotCreateWindow {
             get {
                 return ResourceManager.GetString("CanNotCreateWindow", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace MEFEditor.Plugin {
         /// <summary>
         ///   Looks up a localized string similar to Enhanced MEF Component Architecture Editor.
         /// </summary>
-        internal static string ToolWindowTitle {
+        public static string ToolWindowTitle {
             get {
                 return ResourceManager.GetString("ToolWindowTitle", resourceCulture);
             }
